69b64427db0fd9db03381685d28bea412f30a49c
Kuitcodekt <136946712+kuitcodekt@users.noreply.github.com>
2025-03-09 22:15:51 +0900

    Update time.js

--- api/time.js
+++ api/time.js
@@ -1,16 +1,5 @@
 // api/time.js
  export default function handler(req, res) {
    const now = new Date();
-   // 日本時間のタイムゾーンオフセット（UTC+9時間）
-const japanOffset = 9 * 60;
-
-// 現在のタイムゾーンオフセットを取得（分単位）
-const localOffset = now.getTimezoneOffset();
-
-// 日本時間を計算
-const japanTime = new Date(now.getTime() + (japanOffset + localOffset) * 60 * 1000);
-
-console.log(japanTime.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
-
    res.status(200).json({ time: now.toISOString() });
  }
