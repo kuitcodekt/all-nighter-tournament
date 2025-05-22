81672567e66212ff1360aecfa0e5c67c98a1b1a9
Kuitcodekt <136946712+kuitcodekt@users.noreply.github.com>
2025-05-22 19:50:10 +0900

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
-  'https://YOUR_PROJECT.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
+  'https://eccrioufdqzxkapgeaha.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjY3Jpb3VmZHF6eGthcGdlYWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MDI0OTcsImV4cCI6MjA2MzQ3ODQ5N30.B9mv-_WtYWZkz3-MPFxtaAnrY84veq4iLGzG1qJ2U4w'
 );
